96daa547f9df8a868a6b8ee3809b923dce3f75d0
jheng18 <31415506+jheng18@users.noreply.github.com>
2018-06-04 16:50:28 -0500

    Add files via upload

--- collocation.py
+++ collocation.py
@@ -3,6 +3,7 @@ from mrjob.step import MRStep
 import re
 WORD_RE = re.compile(r"[\w]+")
 
+# clean reference frame input, save it to a list
 f = open('myframe.txt', 'r')
 x = f.readlines()
 f.close()
@@ -18,6 +19,13 @@ REFERENCE = total
 
 class MRCount(MRJob):
 	def mapper(self, _, line):
+		'''
+		This function goes through each line in the text files, counts words
+		and loop over the reference frame and records the co-occurence time
+		of the word in reference frame and the keyword in the text file. It
+		yields a list, which has one more elements than the reference frame
+		(the word counts).
+		'''
 		newline = WORD_RE.findall(line)
 		for word in newline:
 			rvlist = [0] * (len(REFERENCE)+1)
@@ -29,14 +37,34 @@ class MRCount(MRJob):
 			yield word, rvlist
 
 	def combiner(self, word, countstep1):
+		'''
+		Sum up the list with same key word.
+		:param word: word in text file.
+		:param countstep1: the list generated in the mapper.
+		:return: a list.
+		'''
 		sumstep1 = list(countstep1)
 		yield word,[sum(i) for i in zip(*sumstep1)]
 
 	def reducer_pre(self, word, countstep2):
+		'''
+		Sum up the list with same key word.
+		:param word: word in text file.
+		:param countstep2: the list combined in the combiner.
+		:return: a list.
+		'''
 		sumstep2 = list(countstep2)
 		yield word,[sum(i) for i in zip(*sumstep2)]
 
 	def reducer_final(self, word, allnum):
+		'''
+		Divide the second-to-last elements in the list by the first
+		element. Yield a vector with the same length of reference frame for
+		each unique word in the text file.
+		:param word: unique word in the text file.
+		:param allnum: the list generated in the first step.
+		:return: a list.
+		'''
 		final = []
 		mycount = list(allnum)[0]
 		for i in range(len(mycount)):
@@ -46,8 +74,8 @@ class MRCount(MRJob):
 
 	def steps(self):
 		return[
-		MRStep(	
-			mapper = self.mapper, 
+		MRStep(
+			mapper = self.mapper,
 			combiner = self.combiner,
 			reducer = self.reducer_pre),
 		MRStep(reducer = self.reducer_final)]
